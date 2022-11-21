7c27fece3b370fbbfd023fbbfe88ade7dc61df8a
Geane Ferreira Pereira <geane3096@gmail.com>
2022-11-21 17:49:56 -0400

    adicionando a reatividade do alert ao carrinho

--- app.js
+++ app.js
@@ -1,85 +1,87 @@
-
 const vm = new Vue({
-    el:"#app",
-    data:{
-        produtos:[],
-        produto:false,
-        carrinho:[],
+  el: "#app",
+  data: {
+    produtos: [],
+    produto: false,
+    carrinho: [],
+    mensagemAlerta: "Item adicionado",
+    alertaAtivo: true,
+  },
 
+  filters: {
+    numeroPreco(valor) {
+      return valor.toLocaleString("pt-BR", {
+        style: "currency",
+        currency: "BRL",
+      });
+    },
+  },
+  computed: {
+    carrinhoTotal() {
+      let total = 0;
+      if (this.carrinho.length) {
+        this.carrinho.forEach((item) => {
+          total += item.preco;
+        });
+      }
+      return total;
+    },
+  },
+  methods: {
+    fetchProdutos() {
+      fetch("./api/produtos.json")
+        .then((r) => r.json())
+        .then((r) => {
+          this.produtos = r;
+        });
+    },
+    fetchProduto(id) {
+      fetch(`./api/produtos/${id}/dados.json`)
+        .then((r) => r.json())
+        .then((r) => {
+          this.produto = r;
+        });
     },
 
-        filters: {
-            numeroPreco(valor) {
-              return valor.toLocaleString("pt-BR", { style: "currency", currency: "BRL" });
-            }
-          },
-          computed:{
-            carrinhoTotal(){
-                let total = 0
-                if(this.carrinho.length){
-                    this.carrinho.forEach(item => {
-                        total+= item.preco;
-                    });
-                }
-                return total;
-            }
-
-          },
-    methods: {
-        fetchProdutos(){
-            fetch("./api/produtos.json")
-            .then(r => r.json())
-            .then(r =>{
-                this.produtos = r;
-            })
-
-        },
-        fetchProduto(id){
-            fetch(`./api/produtos/${id}/dados.json`)
-            .then(r => r.json())
-            .then( r => {
-                this.produto = r
-            })
-
-        }, 
-        
-        abrirModal(id){
-            this.fetchProduto(id);
-            window.scrollTo({
-                top:0,
-                behavior:"smooth",
-            })
-
-        },
-        fecharModal({target,currentTarget}){
-            if(target === currentTarget)
-                this.produto = false;     
-        },
-        adicionarItem(){
-            this.produto.estoque--;
-            const{id, nome, preco} = this.produto;           
-            this.carrinho.push({id,nome,preco});
-        },
-        removerItem(index){
-            this.carrinho.splice(index,1);
-
-        }  
+    abrirModal(id) {
+      this.fetchProduto(id);
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
     },
-    watch:{
-        carrinho(){
-            window.localStorage.carrinho = JSON.stringify(this.carrinho);
-        },
-        checarLocalStorage(){
-            if(window.localStorage.carrinho){
-                this.carrinho = JSON.parse(window.localStorage.carrinho);
-        
-            }
-
-        }
-
+    fecharModal({ target, currentTarget }) {
+      if (target === currentTarget) this.produto = false;
     },
-    created() {
-        this.fetchProdutos();
-        this.checarLocalStorage();
+    adicionarItem() {
+      this.produto.estoque--;
+      const { id, nome, preco } = this.produto;
+      this.carrinho.push({ id, nome, preco });
+      this.alerta(`${nome} adicionado ao carrinho`);
     },
-})
+    removerItem(index) {
+      this.carrinho.splice(index, 1);
+    },
+  },
+  watch: {
+    carrinho() {
+      window.localStorage.carrinho = JSON.stringify(this.carrinho);
+    },
+    checarLocalStorage() {
+      if (window.localStorage.carrinho)
+        this.carrinho = JSON.parse(window.localStorage.carrinho);
+    },
+    alerta(mesagem) {
+      this.mensagemAlerta = mensagem;
+      this.alertaAtivo = true;
+      setTimeout(() => {
+        this.alertaAtivo = false;
+      }, 1500);
+    },
+  },
+
+  created() {
+    this.fetchProdutos();
+    this.checarLocalStorage();
+  },
+});
