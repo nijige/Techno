45c3df89b30eb8e81face9760b156287419a1af1
Geane Ferreira Pereira <geane3096@gmail.com>
2022-11-22 10:14:09 -0400

    carrinho pronto estilização e modal .

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const vm = new Vue({
     produtos: [],
     produto: false,
     carrinho: [],
+    carrinhoAtivo: false,
     mensagemAlerta: "Item adicionado",
     alertaAtivo: false,
   },
@@ -53,6 +54,11 @@ const vm = new Vue({
     fecharModal({ target, currentTarget }) {
       if (target === currentTarget) this.produto = false;
     },
+
+    clickForaCarrinho({ target, currentTarget }) {
+      if (target === currentTarget) this.carrinhoAtivo = false;
+    },  
+
     adicionarItem() {
       this.produto.estoque--;
       const { id, nome, preco } = this.produto;
